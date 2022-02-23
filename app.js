b7be4176d7e35f644626712fc07e59c175d68a5a
TasnimAhmed <tasnim.ahamed.20@gmail.com>
2022-02-23 08:16:14 +0600

    Hex and RGB color Genarate and copy

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
 /**
  * @Title Change the background color by generating Hex color
- * @feature Add a button to Copy the hex code
- * @steps
- * @step1 => Create onload Handler
- * @step2 => Random Color Generator function
- * @step3 => Collect all necessary references
- * @step4 => Handle the click event
- * @step5 => Handle the click event
- * @step6 => Add a toast message when color copied
- * @step7 => User can Type Hex Code
  */
 
 /**
@@ -21,14 +12,21 @@ function main(){
     const root=document.getElementById('root');
     const changeBtn=document.getElementById('change-color');
     const output=document.getElementById('output');
+    const output2=document.getElementById('output2');
     const copyBtn=document.getElementById('copy-btn');
+    const copyBtn2=document.getElementById('copy-btn2');
     changeBtn.addEventListener('click',function(){
-        const bgColor=generatorHEXColor();
-        output.value=bgColor.toUpperCase();
-        root.style.backgroundColor=bgColor;
+        const decimalColor=generatorDecimalColor();
+        const bgHexColor=generatorHEXColor(decimalColor).toUpperCase();
+        const bgRGBColor=generatorRGBColor(decimalColor);
+        output.value=bgHexColor;
+        output2.value=bgRGBColor;
+        root.style.backgroundColor=bgHexColor;
     })
+
     copyBtn.addEventListener('click',function(){
-        color=output.value;
+
+        const color=output.value;
         if(color[0]==='#'){
             window.navigator.clipboard.writeText(color)
         }
@@ -48,17 +46,29 @@ function main(){
             alert("Invalid Color Code.");
         }
     })
+
+    copyBtn2.addEventListener('click',function(){
+        const color=output2.value;
+        window.navigator.clipboard.writeText(color)
+        generateToastMessage(`${color} copied`)
+    })
+
     output.addEventListener('keyup',function(e){
-        const color=e.target.value;
+        let color=e.target.value;
         if(color){
             output.value=color.toUpperCase();
-            if(color[0]==='#' && isValidHexCode(color)){
-                root.style.backgroundColor=color;
+            if(color[0]==='#'){
+                color=color.substring(1)
             }
-            else{
-                if(isValidHexCode(color)){
-                    root.style.backgroundColor=`#${color}`;
+            if(isValidHexCode(color)){
+                const arrayColor=color.match(/.{1,2}/g);
+                const decimalColor={
+                    red:parseInt(arrayColor[0],16),
+                    green:parseInt(arrayColor[1],16),
+                    blue:parseInt(arrayColor[2],16),
                 }
+                output2.value=generatorRGBColor(decimalColor)
+                root.style.backgroundColor=`#${color}`;
             }
             
         }
@@ -66,22 +76,27 @@ function main(){
     })
 }
 
-
-/**
- * @step2
- */
-
-function generatorHEXColor(){
-    const red= Math.round(Math.random()*255+1).toString(16);
-    const green= Math.round(Math.random()*255+1).toString(16);
-    const blue= Math.round(Math.random()*255+1).toString(16);
-    return `#${red}${green}${blue}`;
+function generatorDecimalColor(){
+    const red= Math.round(Math.random()*255+1);
+    const green= Math.round(Math.random()*255+1);
+    const blue= Math.round(Math.random()*255+1);
+    return {
+        red,
+        green,
+        blue
+    }
+}
+function generatorHEXColor({red, green, blue}){
+    const validHexCode=(color)=>{
+        let hex=color.toString(16);
+        return hex.length==1 ? `0${hex}`:hex;
+    }
+    return `#${validHexCode(red)}${validHexCode(green)}${validHexCode(blue)}`;
+}
+function generatorRGBColor({red, green, blue}){
+    return `rgb(${red}, ${green}, ${blue})`;
 }
 
-
-/**
-*@step6
-*/
 function generateToastMessage(message){
     const div=document.createElement('div')
     div.className='toast-message  toast-message-slide-in';
@@ -108,6 +123,8 @@ function generateToastMessage(message){
     document.body.appendChild(div);
 }
 
+
+
 function isValidHexCode(color){
     if(color[0]=='#'){
         if(color.length!==7) return false;
@@ -117,4 +134,3 @@ function isValidHexCode(color){
     }
     return /^[0-9A-Fa-f]{6}$/i.test(color); //Regex
 }
-
